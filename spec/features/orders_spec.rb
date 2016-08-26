0459b4f6887124a9094691bf9e11f1f9e7483fac
awye765 <awye765@googlemail.com>
2016-08-26 18:58:36 +0100

    Fixed delete action AJAX error on deletion of items in cart.  Items can now be deleted with AJAX

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -6,14 +6,14 @@ feature "Orders Page" do
 
   scenario "can be viewed by clicking on checkout link" do
     visit '/'
-    click_link '0 Items in Cart ( )'
+    click_link '0 Items in Cart (£)'
     expect(current_path).to eq('/order')
   end
 
   scenario "displays the order items in an order", js: true do
     visit '/'
     page.find('btn-primary').click
-    click_link '1 Items in Cart ( 99.0 )'
+    click_link '1 Items in Cart (£99.00)'
     expect(page).to have_content("Almond Toe Court Shoes")
   end
 
