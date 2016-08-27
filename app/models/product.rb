86fde416ca146bccc9550af7b898615073e6f645
awye765 <awye765@googlemail.com>
2016-08-27 11:54:22 +0100

    Added functionality to update stock up or down depending on what is in user's cart.  Edge cases to be fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ActiveRecord::Base
+
+  has_many :order_items
+
   has_attached_file :image, :styles => { :medium => "300x300",
                                          :thumb => "100x100"
                                        }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/                              
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def remove_product_from_stock
+    self.stock -= 1
+    self.save
+  end
+
+  def return_product_to_stock
+    self.stock += 1
+    self.save
+  end
+
 end
