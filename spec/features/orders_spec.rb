660bda37811645c3bb7f6790e324d6da17039306
awye765 <awye765@googlemail.com>
2016-08-29 20:57:07 +0100

    Added additional test to improve test coverage of orders model

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -27,6 +27,16 @@ feature "FEATURE: Shopping Cart" do
     expect(page).to have_content("There are no items in your shopping cart.  Please go back and add some items to your cart")
   end
 
+  scenario "does not accept a negative number for an order item", js: true do
+    visit '/'
+    expect(page).to have_content("Amount left: 5 in stock")
+
+    fill_in "order_item_quantity", with: -1
+    click_on('Add to Cart')
+    
+    expect(page).to have_content("Amount left: 5 in stock")
+  end
+
   # scenario "allows user to increase the quantity of an item in the cart", js: true do
   #   visit '/'
   #   click_on('Add to Cart')
