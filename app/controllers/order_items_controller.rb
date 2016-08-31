32497d5e63ded62b6e7b50a5563f9caa4488f7b3
awye765 <awye765@googlemail.com>
2016-08-31 19:33:50 +0100

    Refactored orders controller, orders model, order_items_controller and order_items_model

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,48 +1,13 @@
 class OrderItemsController < ApplicationController
 
-  def create
-    @order = current_order
-    @order.discount = 0
-    @order_item = @order.order_items.new(order_item_params)
-    @order.save
-    @product = @order_item.product
-    @product.update_attributes(stock: (@product.stock - @order_item.quantity))
-    # @product.remove_product_from_stock
-    @order.update_total
-    session[:order_id] = @order.id
-    @products = Product.all
-    respond_to do | format |
-      format.js
-    end
-  end
+  include OrderItemsHelper
 
-  # def update
-  #    @order = current_order
-  #    @order_item = @order.order_items.find(params[:id])
-  #    @order_item.update_attributes(order_item_params)
-  #    @order_items = @order.order_items
-  #  end
+  def create
+    add_quantity_of_roduct_to_cart
+  end
 
   def destroy
-    @order = current_order
-    @order_item = @order.order_items.find(params[:id])
-    @product = Product.find(@order_item.product.id)
-    @order_item.product.update_attributes(stock: (@order_item.product.stock + @order_item.quantity))
-
-    # @product.return_product_to_stock
-    @order_item.destroy
-    @order_items = @order.order_items
-    @order.update_total
+    remove_quantity_of_product_from_cart
   end
-
-  def order_item_params
-    params.require(:order_item).permit(:quantity, :product_id)
-  end
-
-  # def remove_product_from_stock
-  # end
-  #
-  # def return_product_to_stock
-  # end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,15 +5,27 @@ class Order < ActiveRecord::Base
   attr_accessor :voucher
 
   def update_total
+    update_total_to_subtotal
+    update_total_after_applicable_discount
+    validate_total_greater_than_zero
+    remove_discount
+  end
+
+  def update_total_to_subtotal
     self.total = self.subtotal
+  end
+
+  def update_total_after_applicable_discount
     self.total = self.subtotal - self.discount
+  end
+
+  def validate_total_greater_than_zero
     if self.total < 0
        self.total = 0
        self.save
     else
       self.save
     end
-    remove_discount
   end
 
   def subtotal
@@ -37,11 +49,19 @@ class Order < ActiveRecord::Base
   end
 
   def get_categories
-    products = []
-    categories = []
-    self.order_items.each{|orderitem| products << Product.find(orderitem.product_id)}
-    products.each{|product| categories << product.category}
-    return categories
+    find_product_associated_with_order_item
+    find_product_category_associated_with_product
+    return @categories
+  end
+
+  def find_product_associated_with_order_item
+    @products = []
+    self.order_items.each{|orderitem| @products << Product.find(orderitem.product_id)}
+  end
+
+  def find_product_category_associated_with_product
+    @categories = []
+    @products.each{|product| @categories << product.category}
   end
 
 end
